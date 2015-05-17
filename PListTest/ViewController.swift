08bf355b6bc3d87f18e3ecad371af4abe415398c
Chris Wardman <chrisatdarley@aol.com>
2015-05-17 07:15:57 +0100

    Initial Commit

---
+++ PListTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PListTest
+//
+//  Created by Chris Wardman on 17/05/2015.
+//  Copyright (c) 2015 Chris Wardman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
