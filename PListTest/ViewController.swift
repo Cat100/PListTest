21d53f7a2aca34371b2594b30f30e1f534163077
Chris Wardman <chrisatdarley@aol.com>
2015-05-18 18:36:52 +0100

    small change

--- PListTest/ViewController.swift
+++ PListTest/ViewController.swift
@@ -20,46 +20,47 @@ class ViewController: UIViewController {
     
     }
     
-    func loadPList() {
+    func documentsPath() -> String {
         
         let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory,
             .UserDomainMask,
-            true) as [AnyObject]
+            true)
         let documentsDirectory = paths[0] as! String
-        let documentsPath = documentsDirectory.stringByAppendingPathComponent("anyoldplist.plist")
+        
+        return documentsDirectory.stringByAppendingPathComponent("anyoldplist.plist")
+        
+    }
+    
+    func loadPList() {
+        
         
         let fileManager = NSFileManager.defaultManager()
         
-        if !fileManager.fileExistsAtPath(documentsPath) {
+        if !fileManager.fileExistsAtPath(documentsPath()) {
             if let bundlePath = NSBundle.mainBundle().pathForResource("anyoldplist", ofType: "plist") {
-                fileManager.copyItemAtPath(bundlePath, toPath: documentsPath, error: nil)
+                fileManager.copyItemAtPath(bundlePath, toPath: documentsPath(), error: nil)
             }
         }
         
-        if let dict = NSMutableDictionary(contentsOfFile: documentsPath) {
+        if let dict = NSMutableDictionary(contentsOfFile: documentsPath()) {
             tripNumber = dict.objectForKey("tripCount")
         }
     }
     
     func saveCount(tripNum: Int) {
         
-        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory,
-            .UserDomainMask,
-            true) as [AnyObject]
-        let documentsDirectory = paths[0] as! String
-        let documentsPath = documentsDirectory.stringByAppendingPathComponent("anyoldplist.plist")
         
         let fileManager = NSFileManager.defaultManager()
-        
-        if !fileManager.fileExistsAtPath(documentsPath) {
+        ()
+        if !fileManager.fileExistsAtPath(documentsPath()) {
             
             println("Error")
             
         } else {
             
-            let dict = NSMutableDictionary(contentsOfFile: documentsPath)
+            let dict = NSMutableDictionary(contentsOfFile: documentsPath())
             dict?.setObject(tripNum, forKey: "tripCount")
-            dict?.writeToFile(documentsPath, atomically: false)
+            dict?.writeToFile(documentsPath(), atomically: false)
         }
     }
 }
